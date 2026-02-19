9ac006329fb9a2b31593340dfe0f752305772d45
shindesumit2004 <sarvdnyaagency6547@gmail.com>
2026-02-19 14:49:15 +0530

    created save-endpoint

---
+++ src/main/java/com/CRUD/controller/StudentController.java
@@ -0,0 +1,22 @@
+package com.CRUD.controller;
+
+import com.CRUD.model.Student;
+import com.CRUD.service.StudentService;
+import com.CRUD.service.impl.StudentServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class StudentController {
+    @Autowired
+    private StudentService service;
+    // used to add the new student in db
+    @PostMapping("/addStudent")
+    public ResponseEntity<?> addStudent(@RequestBody Student std){
+       Student student =  this.service.saveStudent(std);
+       return ResponseEntity.status(HttpStatus.OK).body("student saved successfully with id: "+student.getId());
+    }
+}
