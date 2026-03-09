68efdc8ef338453a6201888869af3153d44e1721
Avinash <surwaseavinash85@gmail.com>
2026-03-09 17:56:52 +0530

    Send Message to Student Email

--- src/main/java/com/CRUD/CrudPracticeApplication.java
+++ src/main/java/com/CRUD/CrudPracticeApplication.java
@@ -5,7 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 @EnableCaching
 public class CrudPracticeApplication {
